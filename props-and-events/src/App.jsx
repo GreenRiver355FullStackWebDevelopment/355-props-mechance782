4a368f9e95be1113aa7c26223e1ac85fb75a1c38
Madeleine Chance <mechance782@gmail.com>
2025-04-08 12:35:17 -0700

    import and render RestaurantsContainer

--- props-and-events/src/App.jsx
+++ props-and-events/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
-
+import RestaurantsContainer from "./components/RestaurantsContainer.jsx";
+// import restaurants container to render all restaurants in data array
 function App() {
   return (
     <div className="App">
-
+      <RestaurantsContainer/>
     </div>
   );
 }

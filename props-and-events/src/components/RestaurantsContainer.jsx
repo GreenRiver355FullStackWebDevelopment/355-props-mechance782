4e98d094da4db7bdbd59596aef4d8e1f3262c879
Madeleine Chance <mechance782@gmail.com>
2025-04-08 12:36:26 -0700

    map restaurants array to new Restaurant components and pass data through props

--- props-and-events/src/components/RestaurantsContainer.jsx
+++ props-and-events/src/components/RestaurantsContainer.jsx
@@ -1,7 +1,16 @@
+import Restaurant from "./Restaurant.jsx";
+import { restaurants } from "../data/data.js";
+
+// for each restaurant in data array, create Restuarant component with props from restuarant
 function RestaurantsContainer() {
   return (
     <div className="restaurantContainer">
-
+      {
+        restaurants.map((restaurant) => ( <Restaurant name={restaurant.name} 
+        address={restaurant.address} phone={restaurant.phone} cuisine={restaurant.cuisine}
+        rating={restaurant.rating} image={restaurant.image} hours={restaurant.hours} 
+        menu={restaurant.menu} />))
+      }
     </div>
   );
 }
